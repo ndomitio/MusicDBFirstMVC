c52db2ba36354117ad3d5cebdff223c056bf602f
NicholasDomitio <nicholas.domitio@gmail.com>
2018-03-14 19:20:05 -0400

    endofday

--- MusicDBFirstMVC/Models/Album.cs
+++ MusicDBFirstMVC/Models/Album.cs
@@ -11,17 +11,24 @@ namespace MusicDBFirstMVC.Models
 {
     using System;
     using System.Collections.Generic;
-    
+    using System.ComponentModel.DataAnnotations;
+
     public partial class Album
     {
         public int AlbumID { get; set; }
+        [Display(Name = "Name")]
         public string AlbumName { get; set; }
         public string JamCity { get; set; }
+        [Display(Name = "Date Released")]
+        [DisplayFormat(DataFormatString = "{0:yyyy/MM/dd}")]
         public System.DateTime DateReleased { get; set; }
+        [Display(Name = "Album Art")]
         public string AlbumPhoto { get; set; }
+        [Display(Name = "Track Listing")]
         public string AlbumPhotoBack { get; set; }
+        [Display(Name = "Artist")]
         public int ArtistID { get; set; }
-    
+
         public virtual Artist Artist { get; set; }
     }
 }
